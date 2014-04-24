2eb99a00ffa9098c9b5edc836d61aa15707502e8
uupaa <uupaa.js@gmail.com>
2014-04-25 02:34:17 +0900

    update setting

--- test/index.node.js
+++
@@ -1,9 +0,0 @@
-require("uupaa.valid.js");
-require("uupaa.task.js");
-require("uupaa.test.js");
-require("uupaa.spec.js");
-require("uupaa.useragent.js");
-require("../lib/DevToolsDetector.js");
-require("../release/DevToolsDetector.min.js");
-require("./test.js");
-
